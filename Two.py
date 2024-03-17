0dd75b342322493ce2be5f5b665a3973eef6f636
Serendipity004 <163754079+Serendipity004@users.noreply.github.com>
2024-03-18 01:56:45 +0800

    Update Two.py

--- Two.py
+++ Two.py
@@ -5,11 +5,13 @@ import streamlit as st
 st.title('长三角PM2.5监测数据')
 
 #读取csv数据以dataframe显示
-
 url_data = "https://EcnuGISChaser.github.io/gis_development/data/csj_pm25.csv"
 df = pd.read_csv(url_data,encoding="utf8")
-#st.dataframe(df) 
 
+#创建expander容器
+with st.expander("显示原始数据"):
+    st.DataFrame(df)
+    
 #以地图显示
 df1 = pd.read_csv(url_data,usecols=["经度","纬度"],encoding="utf8")
 df1["lon"] = df1["经度"]
@@ -22,15 +24,13 @@ with st.form("my_form"):
 #一个表单提交按钮form_submit_button
     st.form_submit_button('提交')
 #两个selectbox
-    #第一个“选择一个字段”
+#第一个“选择一个字段”
     name1 = st.multiselect('选择一个字段',names1)
     names1 = list(["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"])
-
-    #第二个“选择一个关系”
+#第二个“选择一个关系”
     name2 = st.multiselect('选择一个字段',names2)
     names2 = list(['>','<'])
-
-    #一个Text_input
+#一个Text_input
     txt = st.text_input('输入一个值',value="",type="default")#type为缺省
     Registration_default = '0'
     if st.button("提交"):
@@ -39,10 +39,3 @@ with st.form("my_form"):
             st.map(df1)
         else:
             st.markdown("错误:heavy_multiplication_x:，请重新输入")
-
-    
-
-#创建expander容器
-with st.expander("显示原始数据"):
-    st.DataFrame(df)
-
